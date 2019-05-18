69bed6c117fd490ffc779b48e1bed8b4dd5d9e7a
g.borodin <g.borodin@rambler-co.ru>
2019-05-19 00:57:00 +0300

    Fix library name

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
 @contextlib.contextmanager
 def noalsaerr():
-    asound = cdll.LoadLibrary('libasound.so')
+    asound = cdll.LoadLibrary('libasound.so.2')
     asound.snd_lib_error_set_handler(c_error_handler)
     yield
     asound.snd_lib_error_set_handler(None)
